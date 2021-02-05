050036a1923c75c3ea88cea19991949a865ac83a
aangupta <aanchalgupta457@gmail.com>
2021-02-05 11:23:35 +0530

    mofified script

--- script.js
+++ script.js
@@ -2,17 +2,13 @@ const input = document.querySelector('#input-text');
 const encodebtn = document.querySelector('#encode');
 const decodebtn = document.querySelector('#decode');
 const clearbtn = document.querySelector('#clear');
-const base64btn = document.querySelector('#base64encode');
 encodebtn.addEventListener('click', () => {
-    document.querySelector('#output-text').value = encodeURI(input.value);
+    document.querySelector('#output-text').value = encodeURIComponent(input.value);
 });
 decodebtn.addEventListener('click', () => {
-    document.querySelector('#output-text').value = decodeURI(input.value);
+    document.querySelector('#output-text').value = decodeURIComponent(input.value);
 });
 clearbtn.addEventListener('click', () => {
     document.querySelector('#output-text').value = '';
     document.querySelector('#input-text').value = '';
-});
-base64btn.addEventListener('click', () => {
-    document.querySelector('#output-text').value = window.btoa(input.value);
 });
